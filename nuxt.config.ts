ece7fa53f614ce4d1141ed930e4d2b8053414874
Gregor Tokarev <dev@gregortokarev.com>
2022-03-27 06:03:45 +0300

    Add font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@ import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ["@/assets/style/main.scss", "reset-css"],
+  css: ["@/assets/style/main.scss", "normalize.css"],
+  typescript: {
+    strict: true,
+  },
+  loading: {
+    color: "#000",
+  },
+  vueMeta: {
+    title: "Gregor's blog",
+  },
 });
