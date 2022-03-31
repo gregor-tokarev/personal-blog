4d531926c0b9e822ccc3e886c0897e953bfde3d0
Gregor Tokarev <dev@gregortokarev.com>
2022-03-31 17:38:21 +0300

    Add title near ava

--- components/Header.vue
+++ components/Header.vue
@@ -3,6 +3,9 @@
     <div class="header__inner container">
       <div class="header__left">
         <img alt="my face" width="60px" height="60px" src="~public/img/my-face.jpg" class="header__face">
+
+        <div class="header__text-mobile content-text">G.T</div>
+        <div class="header__text-desktop content-text">Georg Tokarev - Software engeneer & ux/ui designer</div>
       </div>
       <div class="header__right">
         <!--        <Button class="header__follow" type="gray">Follow</Button>-->
@@ -13,7 +16,6 @@
 </template>
 
 <script setup lang="ts">
-
 import Button from "~/components/Button.vue";
 </script>
 
@@ -22,8 +24,14 @@ import Button from "~/components/Button.vue";
   background-color: var(--white);
   padding: 8px 0;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
+  color: var(--gray-300);
 
   &__inner {
+    display: flex;
+    align-items: center;
+  }
+
+  &__left {
     display: flex;
     align-items: center;
   }
@@ -34,6 +42,23 @@ import Button from "~/components/Button.vue";
 
   &__follow {
     margin-right: 10px;
+  }
+
+  &__text-desktop {
+    display: none;
+    margin-left: 15px;
+
+    @include apply-ps {
+      display: block;
+    }
+  }
+
+  &__text-mobile {
+    margin-left: 5px;
+
+    @include apply-ps {
+      display: none;
+    }
   }
 }
 </style>
