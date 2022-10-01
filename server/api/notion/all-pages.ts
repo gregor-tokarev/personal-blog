c0e977d732238d2b78edd0835ba82bfad10965e0
Gregor Tokarev <dev@gregortokarev.com>
2022-10-01 18:03:19 +0300

    Change order of points

--- server/api/notion/all-pages.ts
+++ server/api/notion/all-pages.ts
@@ -8,7 +8,7 @@ export default async function () {
     sorts: [
       {
         property: "createdDate",
-        direction: "ascending",
+        direction: "descending",
       },
     ],
     filter: {
